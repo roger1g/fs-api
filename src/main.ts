53f87285a05feed510be855fd34e0f7e6f715ae9
kevinkim910408 <junho2413@gmail.com>
2023-08-21 13:19:50 -0400

    fix: db connecton

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,9 @@ import express, { Request, Response } from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import { customerRoute } from "./route/customerRoute";
-import mongoose from "mongoose";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
-mongoose.set("strictQuery", true);
 
 var corsOptions = {
   origin: "*",
@@ -26,31 +25,13 @@ app.get("/", (req: Request, res: Response) => {
   );
 });
 
+connectDB();
+
 app.use("/customers", customerRoute);
 
-const connection = async () => {
-  try {
-    if (!process.env.MONGO_CONNECTION_STRING) {
-      throw new Error(
-        "MONGO_CONNECTION_STRING environment variable is not defined."
-      );
-    }
-
-    await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
-      ignoreUndefined: true,
-    });
-    console.log("MongoDB Connected...");
-
-    app.listen(port, async () => {
-      console.log(`Connected: ${port}`);
-    });
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
-
-connection();
+app.listen(port, async () => {
+  console.log(`Connected: ${port}`);
+});
 
 app.use((req: Request, res: Response) => {
   res.status(404).json({
